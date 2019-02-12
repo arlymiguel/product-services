e45d8f91afab1a760fe3ae230594dd2b5aa64396
Arly Fernandez <S80542@credito.bcp.com.pe>
2019-02-12 18:03:50 -0500

    added crossorigin

--- src/main/java/com/armifella/product/expose/web/ProductController.java
+++ src/main/java/com/armifella/product/expose/web/ProductController.java
@@ -3,6 +3,7 @@ package com.armifella.product.expose.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ public class ProductController {
 		this.productService = productService;
 	}
 	
+	@CrossOrigin
 	@GetMapping("")
 	public List<Product> getAllProduct() {
 		return productService.findAll();
